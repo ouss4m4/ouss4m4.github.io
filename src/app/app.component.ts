d0e7b0dc6faf338911e36ca15be597ac06bc16e3
Oussama <bz.oussama@gmail.com>
2020-06-01 22:03:42 +0100

    added fb pixel ganalytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
+declare let gtag: Function;
+declare let fbq: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styles: [],
 })
 export class AppComponent {
-  title = 'sales-sniper';
+  constructor(private router: Router) {
+    router.events.subscribe((y: NavigationEnd) => {
+      if (y instanceof NavigationEnd) {
+        gtag('config', 'UA-168145101-1', { page_path: y.url });
+        fbq('track', 'PageView');
+      }
+    });
+  }
 }
