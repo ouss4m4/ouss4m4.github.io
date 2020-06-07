515d0cf54e9a6e0f653278622dc70be1d3d2124f
Oussama <bz.oussama@gmail.com>
2020-06-07 21:24:43 +0100

    refUrl updated

---
+++ src/app/pages/links.ts
@@ -0,0 +1,3 @@
+export const links = {
+  tatoo: 'https://f08bb19a9j-o8y9it6z82jtieq.hop.clickbank.net/?tid=1234',
+};
--- src/app/pages/tattoo-removal/tattoo-removal.component.ts
+++ src/app/pages/tattoo-removal/tattoo-removal.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { links } from '../links';
 
 @Component({
   selector: 'app-tattoo-removal',
   templateUrl: './tattoo-removal.component.html',
   styleUrls: ['tattoo-removal.component.scss'],
 })
-export class TattooRemovalComponent implements OnInit {
+export class TattooRemovalComponent {
   constructor() {}
-
-  ngOnInit(): void {}
+  public referUrl = links.tatoo;
 }
