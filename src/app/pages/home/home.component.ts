7de7e904b4a6e38c3edaf76910bbf32a0c86101c
Oussama <bz.oussama@gmail.com>
2020-06-07 23:21:11 +0100

    added logo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent {
   constructor() {}
+  public pages: { path: string; imageUrl: string; summary: string }[] = [
+    {
+      path: '/tattoo-removal',
+      imageUrl: 'assets/img/tattoo-removal/2.png',
+      summary:
+        'Get rid of your embarassing tattoo with this amazing combination of home natural products',
+    },
+    {
+      path: '/ezbattery',
+      imageUrl: 'assets/img/ezbattery/battery.jpg',
+      summary:
+        'Bringing old batteries back to life is simple with EZ Battery Reconditioning™',
+    },
+  ];
 }
